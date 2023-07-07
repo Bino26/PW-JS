03e4fc76c95b6a201351d4a461a017d25e52a19f
Bino26 <hbino1999@gmail.com>
2023-07-07 23:41:09 +0000

    Handle Error

--- Callback and Promises Assignment/5. Greeting Promise/script.js
+++ Callback and Promises Assignment/5. Greeting Promise/script.js
@@ -1,18 +1,16 @@
 function returnName(name) {
   return new Promise(function exec(resolve, reject) {
-    console.log(`Hello,${name}`);
-    if ((name = "")) {
-      reject("No name");
-    } else {
-      resolve("Done");
-    }
+    setTimeout(() => {
+      if (name) {
+        resolve(`Hello ${name}`);
+      } else {
+        reject("Provide a name please !");
+      }
+    });
   });
 }
-let p = returnName();
 
-//statut of the promise
-p.then(function exec(val) {
-  console.log("Promise resolve with a value", val);
-});
-
-returnName("Yann");
+returnName("Yann")
+  .then((message) => console.log(message))
+  .catch((err) => console.log(err));
+// Hello Yann
